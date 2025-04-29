815c3af3751fefbc82fb626dce8f200e1e3d52a7
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-04-29 11:14:33 +0000

    добавил хук useNavigate() для возврата на шаг назад или на главную

--- src/components/pages/Error404.tsx
+++ src/components/pages/Error404.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const Error404 = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            Error404
+        <div style={{ padding: '2rem' }}>
+            <h2>Ошибка 404: Страница не найдена</h2>
+            <p>Извините, запрашиваемая вами страница не существует.</p>
+            <button onClick={() => navigate(-1)}>Назад</button> |{' '}
+            <button onClick={() => navigate('/')}>На главную</button>
         </div>
     );
 };
